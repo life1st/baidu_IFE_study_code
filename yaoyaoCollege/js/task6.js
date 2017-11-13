fab11406aa434396ff9fef6fd9bcc59b4015db30
Yang <jiaoyangwfzc@gmail.com>
2017-11-14 00:16:44 +0800

    update task6 in yaoyaoCollege: now can close window.

--- yaoyaoCollege/js/task6.js
+++ yaoyaoCollege/js/task6.js
@@ -1,22 +1,29 @@
 function FloatWindow(options) {
     var o = options;
-    this.width = o.width || 400;
-    this.height = o.height || 300;
     var node = document.createElement('div');
     node.className = o.class || 'float-window'
     var box = document.createElement('div');
     box.className = 'box';
+    box.style.width = (o.width || '400') + 'px';
+    box.style.height = (o.height || '300') + 'px';
     var header = document.createElement('h2');
     header.innerText = o.title || '浮出层标题';
     box.appendChild(header);
     node.appendChild(box);
+
+    node.addEventListener('click', function (e) {
+        if (e.target.className != 'box') {
+            node.parentNode.removeChild(node);
+        }
+    })
     return node;
 }
 
 var window1 = new FloatWindow({
     width: 500,
     height: 400,
-    title: '这是一个浮出层'
+    title: '这是一个浮出层',
+    btn: ['确定', '取消']
 })
 
 document.querySelector('input[type=button]').onclick = function () {
