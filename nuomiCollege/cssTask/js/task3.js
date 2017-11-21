9c8b2f6d63f272312ff658efe48ebc230129a605
Yang <jiaoyangwfzc@gmail.com>
2017-11-21 10:54:39 +0800

    update cssTask3 in nuomiCollege.

---
+++ nuomiCollege/cssTask/js/task3.js
@@ -0,0 +1,31 @@
+document.querySelector('body').addEventListener('contextmenu',function (e) {
+    e.preventDefault();
+    removeWindow();
+    var pos = {
+        x: e.clientX,
+        y: e.clientY
+    }
+    addWindow(pos);
+    console.log(pos.x, pos.y,'contextmenu add');
+})
+
+document.addEventListener('click', function () {
+    removeWindow();
+})
+function removeWindow() {
+    var box = document.querySelector('.context-menu') || false
+    if(box){
+        document.querySelector('body').removeChild(box)
+    }
+}
+function addWindow(pos) {
+    var box = document.createElement('div')
+    var content =
+        '<p>Menu item 1</p>'+
+        '<p>Menu item 2</p>';
+    box.className = 'context-menu';
+    box.style.top = pos.y + 'px';
+    box.style.left = pos.x + 'px';
+    box.innerHTML = content;
+    document.querySelector('body').appendChild(box);
+}
