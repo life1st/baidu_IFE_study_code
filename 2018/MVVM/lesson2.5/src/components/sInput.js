46eff416d82f4a36ca79e3e35ae5f8621b55752e
life1st <jiaoyangdev@gmail.com>
2018-04-30 02:12:49 +0800

    update MVVM lesson2.5

--- 2018/MVVM/lesson2.5/src/components/sInput.js
+++ 2018/MVVM/lesson2.5/src/components/sInput.js
@@ -1,5 +1,40 @@
+console.log('sinputer loaded.')
+
 import san from 'san'
+import '../assets/style/inputer.less'
 
 export default san.defineComponent({
+  template: `
+  <div class='inputer'>
+    <p class="alert" s-if="!!alert">
+      <span class="content">{{alert}}</span>
+    </p>
+    <input 
+    class="{{!!warning ? 'warning' : ''}}"
+    type="text" 
+    value="{= value =}"
+    disabled="{{disabled ? 'true' : '' }}"
+    readonly="{{readonly ? 'ture' : ''}}"
+    placeholder="{{placeholder}}"
+    on-focus="emitFocus" 
+    on-blur="emitBlur"
+    on-input="emitInput">
+    <span class="warning-text" s-if="!!warning">{{warning}}</span>
+  </div>
+  `,
 
+  emitBlur(e) {
+    this.fire('blur', e)
+  },
+  emitFocus(e) {
+    this.fire('focus', e)
+  },
+  emitInput(e){
+    this.fire('input', e)
+  },
+  initData() {
+    return {
+      msg: `I'm inputer.`,
+    }
+  }
 })
--- 2018/MVVM/lesson2.5/src/index.js
+++ 2018/MVVM/lesson2.5/src/index.js
@@ -3,35 +3,61 @@ console.log('success load mainjs')
 import './assets/style/common.less'
 import san, { DataTypes } from 'san'
 
-import parent from './components/parent'
+import sInputer from './components/sInput'
 
 let App = san.defineComponent({
   components: {
-
+    's-inputer': sInputer
   },
 
-  template: 
-  `
+  template:
+    `
   <div class='root'>
     <h1>Hello San.</h1>
     <p>the first san app. Ver {{version}}</p>
-    <div class='lesson7'>
-      <div class='result'>
-        <span s-if='!msg'>waiting for msg from child.</span>
-        <span s-else>msg recived:{{msg}}</span>
+    <div class='lesson8'>
+      <div class="input">
+        <div class="title">
+          <div class="info">
+            <span class="name">输入框</span>
+            <div class="block">
+              <p class="type">控件</p>
+              <p class="name-en">Input</p>
+            </div>
+          </div>
+        </div>
+        <div class="content">
+          <h2>基础状态</h2>
+          <s-inputer placeholder="{{placeholder}}"
+          on-input="handleInput"
+          on-focus="handleFocus"
+          on-blur="handleBlur"
+          disabled="true"
+          alert="{{'提示文字'}}"
+          warning="{{'错误提示文字'}}"
+          <!--readonly="true"-->
+          value="{= value =}" />
+        </div>        
       </div>
-      <parent on-bubbing='handleMsg' />
     </div>
   </div>
   `,
-  handleMsg(msg) {
-    console.log('mainJS', msg)
-    this.data.set('msg', msg)
+  handleBlur(e) {
+    console.log('handle blur', e)
+  },
+  handleFocus(e) {
+    console.log('handle focus', e)
+  },
+  handleInput(e) {
+    console.log('handle input', e)
   },
   initData() {
     return {
       version: '0.2.4',
-      msg: ''
+      msg: '',
+      alert: '提示文字',
+      placeholder: 'this is a inputer.',
+      value: 'inputer.'
     }
   }
 })
