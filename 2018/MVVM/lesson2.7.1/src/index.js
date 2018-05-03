4b00af2d5100986ae2e1334743c02b1d1d3ca1d5
life1st <jiaoyangdev@gmail.com>
2018-05-03 16:04:25 +0800

    add MVVM 2.7.1.

---
+++ 2018/MVVM/lesson2.7.1/src/index.js
@@ -0,0 +1,58 @@
+console.log('success load mainjs')
+
+import './assets/style/common.less'
+import san, { DataTypes } from 'san'
+import root from './root.san'
+
+let App = san.defineComponent({
+  components: {
+    root
+  },
+
+  template:
+    `
+  <div class='root'>
+    <h1>Hello San.</h1>
+    <p>the first san app. Ver {{version}}</p>
+    <div class='lesson10'>
+      <root></root>
+    </div>
+  </div>
+  `,
+  initData() {
+    return {
+      version: '0.2.7.1',
+      tasks: [
+        {
+          title: '第一个task',
+          content: [
+            '列表中的第一项',
+            '列表中的第二项',
+            '列表中的第三项',
+            '列表中的第四项',
+          ]
+        },
+        {
+          title: '第二个task',
+          content: [
+            '列表中的第一项',
+            '列表中的第二项',
+            '列表中的第三项',
+            '列表中的第四项',
+          ]
+        },
+        {
+          title: '第三个task',
+          content: [
+            '列表中的第一项',
+            '列表中的第二项',
+            '列表中的第三项',
+            '列表中的第四项',
+          ]
+        },
+      ]
+    }
+  }
+})
+let myApp = new App()
+myApp.attach(document.body)
