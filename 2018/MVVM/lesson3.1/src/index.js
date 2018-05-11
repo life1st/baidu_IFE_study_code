da8550a2d9244590c1e5867c24009b3e584ff0f7
life1st <jiaoyangdev@gmail.com>
2018-05-11 18:55:56 +0800

    update MVVM 3.3

---
+++ 2018/MVVM/lesson3.1/src/index.js
@@ -0,0 +1,30 @@
+console.log('success load mainjs')
+
+import './assets/style/common.less'
+import san, { DataTypes } from 'san'
+
+import root from './root.san'
+
+let App = san.defineComponent({
+  components: {
+    root
+  },
+
+  template:
+    `
+  <div class='main'>
+    <h1>Hello San.</h1>
+    <p>the first san app. Ver {{version}}</p>
+    <div class='lesson31'>
+      <root />
+    </div>
+  </div>
+  `,
+  initData() {
+    return {
+      version: '0.2.9',
+    }
+  }
+})
+let myApp = new App()
+myApp.attach(document.body)
